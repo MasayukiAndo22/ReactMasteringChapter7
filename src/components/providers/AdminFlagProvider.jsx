a44ba3921ccf8cd48e19476dfca0d96bbe774898
MasayukiAndo <m-ando@mi-labo.co.jp>
2023-02-22 02:13:09 +0000

    Update AdminFlagProvider.jsx and index.js 作成した Context の Provider でグローバル State を扱いたいコンポーネントで囲む

--- src/components/providers/AdminFlagProvider.jsx
+++ src/components/providers/AdminFlagProvider.jsx
@@ -1,3 +1,18 @@
 import { createContext } from "react";
 
 export const AdminFlagContext = createContext({});
+
+export const AdminFlagProvider = props => {
+    const { children } = props;
+    
+    // 動作確認のためのオブジェクト
+    const sampleObj = { sampleValue: "サンプル" };
+    
+    // AdminFlagContext の中にprovider があるのでそれで children を囲む
+    // value のなかにグローバルに扱う実際の値を設定
+    return (
+        <AdminFlagContext.Provider value={sampleObj}>
+            {children}
+        </AdminFlagContext.Provider>
+    );
+};
