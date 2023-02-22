e4b274017581be8e4d38dc1736d068240687ea19
MasayukiAndo <m-ando@mi-labo.co.jp>
2023-02-22 02:17:51 +0000

    Update EditButton.jsx State を参照したいコンポーネントで React.useContext を使う

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,3 +1,9 @@
+// 
+import { useContext } from "react";
+
+// 作成したContext を import
+import { AdminFlagContext } from "./providers/AdminFlagProvider";
+
 const style = {
   width: "100px",
   padding: "6px",
@@ -6,6 +12,10 @@ const style = {
 
 export const EditButton = (props) => {
   const { isAdmin } = props;
+  
+  // useContext の引数に参照する Context を指定する
+  const contextValue = useContext(AdminFlagContext);
+  console.log(contextValue);
 
   return (
     <button style={style} disabled={!isAdmin}>
